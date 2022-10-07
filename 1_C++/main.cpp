e5b5b33a1f6a5fa64d47da9ab11d9f17de58140e
nileshb-15 <115218011+nileshb-15@users.noreply.github.com>
2022-10-07 16:21:06 +0530

    Add files via upload

---
+++ 1_C++/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /*
+  int a =123;
+  cout << a << endl;
+
+  char ch='a';
+  cout<< ch<<endl;
+
+  float c=1.892;
+  cout <<c<<endl;
+
+  int b= sizeof(a);
+  cout << b<< endl;
+ 
+
+  char ch=123456;
+  cout<<ch<<endl;
+
+  */
+
+  
+
+ }
